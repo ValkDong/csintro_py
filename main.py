fc74275576db22b3017ee4f5ded20216868449c2
64066 <153511782+64066@users.noreply.github.com>
2023-12-11 21:42:30 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,21 @@
-import pygame
-import sys
-from globals import *
-from scene import Scene
+    self scene=Scene(self)
+def run(self):
+    while self.running:
+        self.update()
+        self.draw()
+    self.close()
+def update(self):
+    EventHandler.poll_events()
+    for event in EventHandler.events:
+        if event.type==pygame QUIT:
+            self.running=False
 
-class Game:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
-        self.clock = pygame.time.Clock()
-    
-        self.running =True
+    self scene.update()
 
-        self.scene = Scene(self)
-    def run(self):
-        while self.running:
-            self.update()
-            self.draw()
-        self.close()
-    def update(self): 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running =False
-
-        self.scene.update()
-
-        pygame.display.update()
-        self.clock.tick(FPS)
-    def draw(self):
-        self.scene.draw()
-    def close(self):
-        pygame.quit()
-        sys.exit()
-
-if __name__=="__main__":
-    game = Game()
-    game.run()
+    pygame.display.update()
+    self.clock.tick(FPS)
+def draw(self):
+    self.scene.draw()
+def close(self):
+    pygame.quit()
+    sys.exit()
