e26645fb09e735e74815895237ee77f99ed6ea44
y-no1 <152680866+y-no1@users.noreply.github.com>
2023-12-25 11:08:40 +0800

    Update sprite.py

--- sprite.py
+++ sprite.py
@@ -8,4 +8,4 @@ class Entity(pygame.sprite.Sprite):
         self.image = image
         self.rect = self.image.get_rect(topleft = position)
     def update(self):
-        self.rect.x += 1
+        pass
